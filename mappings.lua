4e50f9963cb117ee92d86709aebc3a1c01b349ae
nabeshi-maz <shuji.watanabe@mazrica.com>
2023-05-28 15:39:22 +0900

    add plugin hop.nvim

--- mappings.lua
+++ mappings.lua
@@ -1,9 +1,15 @@
+-- place this in one of your configuration file(s)
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+
 return {
   -- first key is the mode
   n = {
     ["<leader>a"] = { "ggVG", desc = "Select All" },
     -- quick save
     ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- hop
+    ["f"] = { ":HopWord<Enter>", remap=true},
   },
   t = {
     -- setting a mapping to false will disable it
